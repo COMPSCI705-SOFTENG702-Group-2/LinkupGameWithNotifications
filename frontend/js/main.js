025f2dbf6ee616a57cf7ec6caecb2d3283fb5a84
HurinHu <hurin@live.ca>
2020-09-21 23:53:48 +1200

    add calculation

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -36,12 +36,26 @@ function store_points_variable(){
 function stop_storing_points_variable(){
   webgazer.params.storingPoints = false;
 }
+function calculate(arr){
+	var tmp = [];
+	var ratio = 0.2;
+	var height = $(window).height();
+	var p = height*ratio;
+	for(var i=0;i<30;i++){
+		if(arr[1][i]<=p){
+			tmp.push(1);
+		}else{
+			tmp.push(0);
+		}
+	}
+	return tmp;
+}
 var data = [];
 function record(){
 	  store_points_variable();
 	  sleep(5000).then(() => {
 		stop_storing_points_variable();
-		data.push(webgazer.getStoredPoints());
+		data.push(calculate(webgazer.getStoredPoints()));
 	  });
 }
 var notify_close = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
@@ -51,7 +65,7 @@ function showNotify(){
 	$('#msg').text(notify[notify_index]);
 	record();
 	$('.notification').slideDown();
-	sleep(5000).then(() => {
+	sleep(3000).then(() => {
 		$('.notification').slideUp();
 		notify_index+=1;
 	});
